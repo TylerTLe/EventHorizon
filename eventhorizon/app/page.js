15a32b8fdb746c07ef6386152101238afb441b20
TylerTLe <tyler.le@edu.sait.ca>
2023-12-09 00:59:08 -0700

    first commit

---
+++ eventhorizon/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="bg-white flex items-center justify-center">
+      <h1 className="text-black">Ticket system</h1>
+    </div>
+  );
+}
